db5aef7b49e79d113376baf9e4108e2c4c910359
david retzlaff <davids.retzlaff@gmail.com>
2021-04-28 10:33:44 -0300

    set select extractCard period

--- AngularJs/js/app/controllers.js
+++ AngularJs/js/app/controllers.js
@@ -55,33 +55,23 @@ angular.module("app")
 		$scope.debtHistorys = response.data;
 	});
 	});
+
+	
+	  
 })
 
 
 
 //  .controller('ExampleController', function($scope,$http) {
-// 	$http.get('http://localhost:5000/api/period/')
-// 	.then(function(response){	
-		
-// 		$scope.data = {
-// 			model: null,
-// 			availableOptions: response.data
-// 		   };		
-// 	});   
+	
 // })
 
 
-// .controller('langCtrl', function($scope, $location,$filter/*, $translate*/) {
-// 	$scope.switchLanguage = function() {	  		  
-	
-// 	  }	  //$translate.use(langKey);
-// 	}
-//   )
+
 .controller("extractCreditController",function ($scope, $http, $routeParams) {
 	$scope.titulo = "Extrato Cartão de crédito";
 	
 	$scope.customerId = $routeParams.customerId;
-console.log("$scope.customerIds");
 	$http.get('http://localhost:5000/api/account/'+$scope.customerId)	
 	.then(function(response){		
 		$http.get('http://localhost:5000/api/creditcard/'+response.data.id)
@@ -90,5 +80,19 @@ console.log("$scope.customerIds");
 		console.log($scope.creditCard);
 	});
 	});
+
+	// $scope.switchLanguage = function() {	
+	// 	console.log($scope.creditCard);
+	// 	// console.log($scope.data.model);
+	// }
+
+	$http.get('http://localhost:5000/api/period/')
+	.then(function(response){	
+		
+		$scope.data = {
+			model: null,
+			availableOptions: response.data
+		   };		
+	}); 
 })
 
